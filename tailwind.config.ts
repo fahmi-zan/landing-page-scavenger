0ec057e43c118eb147a49638e824165372b30cfe
Fahmi Fauzan <muhamadfahmi.f@gmail.com>
2024-11-04 23:06:16 +0700

    initial source code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
@@ -14,6 +17,22 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [typography, daisyui],
+  daisyui: {
+    base: true,
+    styled: true,
+    utils: true,
+    logs: true,
+    themeRoot: ":root",
+    theme: [
+      {
+        mytheme: {
+          primary: "var(--primary-color)",
+          secondary: "var(--secondary-color)",
+          "base-100": "var(--base-color)",
+        },
+      },
+    ],
+  },
 };
 export default config;
